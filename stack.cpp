07b9b6fb9edce9ca1c1693db169697c45f84bce6
masumrahmanatul <143866312+masumrahmanatul@users.noreply.github.com>
2025-06-17 20:25:10 +0600

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+#define MAX_SIZE 101
+int A[MAX_SIZE];
+int top = -1;
+
+void Push(int x)
+{
+    if(top == MAX_SIZE - 1)
+    {
+        cout<<"Stack Overflow."<<endl;
+        return;
+    }
+    else
+    {
+        top++;
+        A[top] = x;
+    }
+}
+
+void Pop()
+{
+    if(top == -1)
+    {
+        cout<<"Can't pop an element."<<endl;
+        return;
+    }
+    else
+    {
+        top--;
+    }
+}
+
+int Top()
+{
+    cout<<A[top]<<endl;
+}
+
+bool IsEmpty()
+{
+    return top == -1;
+}
+
+void Print()
+{
+    cout<<"Stack: ";
+    for(int i = 0; i<=top; i++)
+    {
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    IsEmpty();
+    Push(4);Print();
+    Push(3);Print();
+    Push(2);Print();
+    Push(1);Print();
+    IsEmpty();
+    Top();
+    Pop();
+    Top();
+    Print();
+}
+
