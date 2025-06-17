766750fb1a6a0b7a9c604d468bf80adf89ead591
masumrahmanatul <143866312+masumrahmanatul@users.noreply.github.com>
2025-06-17 20:45:51 +0600

    linked list implementation of stack

---
+++ linked_list_imp_stack.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+struct Node* top = NULL;
+
+void Push(int x)
+{
+    struct Node* temp = new Node();
+    temp->data = x;
+    temp->next = top;
+    top = temp;
+}
+
+void Pop()
+{
+    struct Node *temp;
+    if(top == NULL)
+    {
+        return;
+    }
+    temp = top;
+    top = top->next;
+    free(temp);
+}
